b4de8e3e445f479fa55e752b30dc173ce0fa62eb
kasi <ht333333@sina.com>
2018-06-07 06:39:31 +0800

    首次上传

---
+++ src/main/java/secondcom/kasi/demo/util/TestUtils.java
@@ -0,0 +1,16 @@
+package secondcom.kasi.demo.util;
+
+/**
+ * ${TODO}
+ *
+ * @Author: HuangTao
+ * @Date: 2018-06-07 06:38
+ * @Version:
+ */
+
+public class TestUtils {
+    public String getString(String name){
+        String newName = "hello " + name;
+        return newName;
+    }
+}
